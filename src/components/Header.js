5b837d3e197415b58d1af3bb0b6f738aad16447d
Brady Camp <bradyjcamp@gmail.com>
2022-08-09 16:27:31 -0700

    Added navbar layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-class Header extends React.Component {
-  render() {
+function Header() {
     return (
-      <header className="text-center">
-        <h1>Future Portfolio page for me!</h1>
-      </header>
+      <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Brady Camp</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-end flex-grow-1 pe-3">
+            <NavDropdown title="Explore" id="basic-nav-dropdown">
+              {/*href's will need to be changed to jump to section of page */}
+              <NavDropdown.Item href="#action/3.1">About</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Projects</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Contact</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
-  }
 }
-
 export default Header;
