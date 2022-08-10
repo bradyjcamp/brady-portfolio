6d7c79c76a3eca593ad62eb4378e8c561a08e9be
Brady Camp <bradyjcamp@gmail.com>
2022-08-10 14:37:23 -0700

    Added Navi Component Folder

---
+++ src/components/Navi/Navi.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Navi = () => {
+    return (
+      <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Brady Camp</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-end flex-grow-1 pe-3">
+            <NavDropdown title="Explore" id="basic-nav-dropdown">
+              {/*href's will need to be changed to jump to section of page */}
+              <NavDropdown.Item href="#action/3.1">About</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Projects</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Contact</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    );
+}
+export default Navi;
